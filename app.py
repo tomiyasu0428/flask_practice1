960e78c888e59af1b89264d3e0b80f3a63456c50
tomiyasu0428 <aolarena16@gmail.com>
2024-12-14 17:17:39 +0900

    flask_pra_1

--- app.py
+++ app.py
@@ -1,6 +1,96 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from config import User
 
 app = Flask(__name__)
+
+
+app.secret_key = "secret"  # 秘密鍵
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_by_id(user_id)
+
+
+# ログインしていないとアクセスできないページにアクセスがあった場合の処理
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    # return redirect("/login")
+    return redirect(url_for("login"))
+
+
+# url_forを使用してURLを生成(login関数の名前を指定する)
+# url_for("login")
+# 生成されるURLは /login
+
+
+# ユーザー登録フォームの表示・登録処理
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        if not request.form["name"] or not request.form["password"] or not request.form["email"]:
+            flash("未入力の項目があります。")
+            return redirect(request.url)
+        if User.select().where(User.name == request.form["name"]):
+            flash("その名前はすでに使われています。")
+            return redirect(request.url)
+        if User.select().where(User.email == request.form["email"]):
+            flash("そのメールアドレスはすでに使われています。")
+            return redirect(request.url)
+        # if get_flashed_messages():
+        #     return redirect(request.url)
+
+        User.create(
+            name=request.form["name"],
+            email=request.form["email"],
+            password=generate_password_hash(request.form["password"]),
+        )
+        return render_template("index.html")
+    return render_template("register.html")
+
+
+# ログインフォームの表示
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # データの検証
+        if not request.form["password"] or not request.form["email"]:
+            flash("未入力の項目があります。")
+            return redirect(request.url)
+
+        # ここでユーザーを認証し、OKならログインする
+        user = User.select().where(User.email == request.form["email"]).first()
+        if user is not None and check_password_hash(user.password, request.form["password"]):
+            login_user(user)
+            flash(f"ようこそ！ {user.name} さん")
+            return redirect(url_for("index"))
+
+        # NGならフラッシュメッセージを設定
+        flash("認証に失敗しました")
+
+    return render_template("login.html")
+
+
+# ログアウト処理
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("ログアウトしました！")
+    return redirect(url_for("index"))
+
+
+# ユーザー削除処理
+@app.route("/unregister")
+@login_required
+def unregister():
+    current_user.delete_instance()
+    logout_user()
+    return redirect(url_for("index"))
 
 
 @app.route("/")
